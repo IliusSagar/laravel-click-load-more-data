8ceb3ef5bd03d1aff167db069ce4afad90b72f67
IliusSagar <sagar01830@gmail.com>
2024-01-29 11:56:37 +0600

    Post Factory Faker

--- resources/views/Note.blade.php
+++ resources/views/Note.blade.php
@@ -1,3 +1,8 @@
 ** composer create-project laravel/laravel example-app
 ** php artisan make:model Post -m
 ** php artisan migrate
+** php artisan make:factory PostFactory --model=Post 
+** factories/PostFactory.php** 
+** php artisan tinker
+    - App\Models\Post::factory()->count(50)->create();
+** php artisan make:controller PostController
