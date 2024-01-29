eb1ae24816f5439f5f82f112694525d09acafe77
IliusSagar <sagar01830@gmail.com>
2024-01-29 13:38:08 +0600

    auto load ajax fetch finally jquery wise done

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,8 +7,16 @@ use App\Models\Post;
 
 class PostController extends Controller
 {
-    public function index(){
+    public function index(Request $request){
         $posts = Post::paginate(6);
+        // return view('posts', compact('posts'));
+
+        if($request->ajax()) {
+            $view = view('data', compact('posts'))->render();
+
+            return response()->json(['html' => $view]);
+        }
+
         return view('posts', compact('posts'));
     }
 }
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Laravel Click Load</title>
 
-    <script src="https://code.jquery.com/jquery-3.7.1.slim.js" integrity="sha256-UgvvN8vBkgO0luPSUl2s8TIlOSYRoGFAX4jlCIm9Adc=" crossorigin="anonymous"></script>
+   
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   </head>
@@ -40,35 +40,37 @@
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
-    <script>
-      var ENDPOINT = "{{ route('posts.index')}}";
-      var page = 1;
-
-      $(".load-more-data").click(function(){
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
+<script>
+    var ENDPOINT = "{{ route('posts.index') }}";
+    var page = 1;
+   
+    $(".load-more-data").click(function(){
         page++;
         LoadMore(page);
-      });
-
-      function LoadMore(page){
-        // alert(page);
-
+    });
+    function LoadMore(page) {
         $.ajax({
-          url: ENDPOINT + "?page=" + page,
-          datatype: "html",
-          type: "get",
-          beforeSend: function(){
-            $('.auto-load').show();
-          }
-        })
-        .done(function (response) {
-          console.log(response);
-        })
-        .fail(function (jqXHR, ajaxOptions, thrownError){
-          console.log('Server error occured');
-        })
-
-      }
-    </script>
-
+                url: ENDPOINT + "?page=" + page,
+                datatype: "html",
+                type: "get",
+                beforeSend: function () {
+                    $('.auto-load').show();
+                }
+            })
+            .done(function (response) {
+                console.log(response);
+                if (response.html == '') {
+                    $('.auto-load').html("End :(");
+                    return;
+                }
+                $('.auto-load').hide();
+                $("#data-wrapper").append("<div class='row'>" + response.html + "</div>");
+            })
+            .fail(function (jqXHR, ajaxOptions, thrownError) {
+                console.log('Server error occured');
+            });
+    }
+</script>
   </body>
 </html>
