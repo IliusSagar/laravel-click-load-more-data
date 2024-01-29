09a5970c229bf95da34835ba83ac3def26efecad
IliusSagar <sagar01830@gmail.com>
2024-01-29 13:24:03 +0600

    bootstrap spinner loading added

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -26,12 +26,21 @@
             <button class="btn btn-success load-more-data">Load More Data...</button>
         </div>
 
+        <div class="auto-load text-center" style="display: none;">
+          <div class="d-flex justify-content-center">
+            <div class="spinner-border" role="status">
+              <span>Loading...</span>
+            </div>
+          </div>
+        </div>
+
     </div>
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
     <script>
+      var ENDPOINT = "{{ route('posts.index')}}";
       var page = 1;
 
       $(".load-more-data").click(function(){
@@ -40,7 +49,23 @@
       });
 
       function LoadMore(page){
-        alert(page);
+        // alert(page);
+
+        $.ajax({
+          url: ENDPOINT + "?page=" + page,
+          datatype: "html",
+          type: "get",
+          beforeSend: function(){
+            $('.auto-load').show();
+          }
+        })
+        .done(function (response) {
+          console.log(response);
+        })
+        .fail(function (jqXHR, ajaxOptions, thrownError){
+          console.log('Server error occured');
+        })
+
       }
     </script>
 
