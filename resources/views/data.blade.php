c6feb130e9da9b44677b4db889337196b05ebb63
IliusSagar <sagar01830@gmail.com>
2024-01-29 13:03:52 +0600

    no-image link with include blade

---
+++ resources/views/data.blade.php
@@ -0,0 +1,9 @@
+<div class="col-4">
+    <div class="card">
+        <img src="https://via.placeholder.com/400x250" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">1 : Laravel Crud Rest Api</h5>
+            <p class="card-text">Sample Description</p>
+        </div>
+    </div>
+</div>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -9,7 +9,21 @@
   <body>
 
 
-    <h1>Laravel Click Load!</h1>
+    <div class="container mt-5">
+
+        <h1>Laravel Click Load!</h1>
+
+        <div id="data-wrapper">
+            <div class="row">
+                @include('data')
+            </div>
+        </div>
+
+        <div class="row text-center" style="padding: 20px;">
+            <button class="btn btn-success load-more-data">Load More Data...</button>
+        </div>
+
+    </div>
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
